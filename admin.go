f89dde9c3bfc9462803a07f76da9e5a4f41be749
Sunlis <seandohring@google.com>
2015-12-06 18:46:07 -0800

    Finally getting a handle on how templating works

--- admin.go
+++ admin.go
@@ -1,17 +1,34 @@
 package edm
 
 import (
-    "fmt"
-    "net/http"
+  "net/http"
+  "text/template"
+  "os"
 )
 
 func init() {
-    http.HandleFunc("/admin", handleAdmin)
+  http.HandleFunc("/admin", handleAdmin)
 }
 
+// type Admin struct {
+//   Check string
+//   Blob string
+// }
+
+const test = `
+This should work
+`
+
 func handleAdmin(w http.ResponseWriter, r *http.Request) {
-    if (r.FormValue("foo") != "") {
-      fmt.Fprint(w, r.FormValue("foo"))
-    }
-    http.ServeFile(w, r, "admin.html")
+  t, err := template.ParseFiles("admin.html")
+  if err != nil {
+    os.Exit(1)
+  }
+
+  // data := Admin {}//r.FormValue("check"), r.FormValue("blob")}
+
+  err = t.Execute(w, nil)
+  if err != nil {
+    os.Exit(1)
+  }
 }
